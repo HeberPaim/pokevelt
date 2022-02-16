12e4f04be4921d581fbee350c81c6c5ed26f6431
Hereberz <heber.joan@gmail.com>
2022-02-16 16:54:50 -0300

    fix to title of HomePage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,7 +21,7 @@ fetchPokemon();
     <title> Pokedex </title>
 </svelte:head>
 
-<h1 class="text-white text-4xl text-center my-8 ">Svelte Kit Pokedexzada</h1>
+<h1 class="text-4xl text-center my-8 ">SvelteKit Pokedex Demo</h1>
 <input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" type="text" bind:value = {searchTerm} placeholder="Search Pokemon">
 <div class="py-4 grid gap-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
     
